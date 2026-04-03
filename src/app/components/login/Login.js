7f69f3a6aabd1bde22262227ea9e239f456d95f2
jiri <bjiri140@gmail.com>
2026-04-03 23:24:44 +0200

    test

--- src/app/components/login/Login.js
+++ src/app/components/login/Login.js
@@ -61,7 +61,7 @@ export default function Login({setIsLogIn}){
                     <input
                         id="passwordInput" 
                         type="password" 
-                        placeholder="Your Password"
+                        placeholder="Your Password ffff"
                         onChange={(e) => setPassword(e.target.value)}
                         required
                     />
